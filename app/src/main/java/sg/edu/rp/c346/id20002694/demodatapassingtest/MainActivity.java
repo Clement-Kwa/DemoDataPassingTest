e84d099fb06bbf7e28a702df9c8c1a530192ab82
20002694 <20002694@myrp.edu.sg>
2021-05-20 12:05:07 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20002694/demodatapassingtest/MainActivity.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c346.id20002694.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id. button1);
+        button2 = findViewById(R.id. button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("char", "a");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id20002694/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,26 @@
+package sg.edu.rp.c346.id20002694.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        tv1 = findViewById(R.id. tv1);
+        Intent intentReceived=getIntent();
+        char charReceived = intentReceived.getCharExtra("char", 'a');
+        tv1.setText("Character value received is "+ charReceived);
+
+    }
+}
